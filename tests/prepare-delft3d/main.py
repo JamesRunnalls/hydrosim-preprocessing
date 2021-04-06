9f3c4b47528112d2efb52e4466d957f18012edbc
JamesRunnalls <james.runnalls@eawag.ch>
2021-04-06 20:57:00 +0200

    Progress

---
+++ tests/prepare-delft3d/main.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+import hydrosim.delft3d as d3d
+import json
+
+
+# Define parameters
+meteolakes_simulation_folder = "./example-sim"
+output_simulation_folder = "./output-sim"
+cosmo_folder = "./demo-data/Meteodata"
+start_date = datetime(2021, 1, 1)
+end_date = datetime(2021, 1, 4)
+grid = {"minx": 490000, "miny": 110000, "maxx": 570000, "maxy": 160000, "dx": 1000, "dy": 1000}
+with open("river-parameters.json") as json_file:
+    river_parameters = json.load(json_file)
+
+# Copy previous simulation files
+d3d.copy_simulation_input_files(meteolakes_simulation_folder, output_simulation_folder)
+start_date = d3d.copy_simulation_restart_file(meteolakes_simulation_folder, output_simulation_folder, start_date)
+
+# Create river data
+cosmo_files = d3d.list_cosmo_files(cosmo_folder, start_date, end_date)
+
+coordinates = list(map(lambda x: x["coordinates"], river_parameters["inflows"]))
+air_temperature = d3d.cosmo_point_timeseries(coordinates, "T_2M", cosmo_files)
+
+# Create meteo files
+d3d.create_meteo_files(output_simulation_folder, cosmo_files, grid)
+
+
+
